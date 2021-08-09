320230f5113b094ed47fcee576fe0c1b874e44be
Julia Martsenko <jmartsenko@gmil.com>
2021-08-09 12:45:47 +0300

    created interfase

---
+++ calculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Yuliya Martsenko on 07.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var resultLable: UILabel!
+    @IBAction func pushACButton(_ sender: UIButton) {
+    }
+    @IBAction func pushNegButton(_ sender: UIButton) {
+    }
+    @IBAction func pushNumberButton(_ sender: UIButton) {
+    }
+    @IBAction func pushEqualityButton(_ sender: UIButton) {
+    }
+    @IBAction func pushOperationButton(_ sender: UIButton) {
+    }
+    
+    func updateViewFromModel() {
+    }
+}
+
